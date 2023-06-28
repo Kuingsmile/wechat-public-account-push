a0d8798ae8cc68e98ef6da6b7af535b7b2b560e2
萌萌哒赫萝 <ma_shiqing@163.com>
2023-06-27 19:34:30 -0700

    添加用户

--- config/index.cjs
+++ config/index.cjs
@@ -28,11 +28,41 @@ const USER_CONFIG = {
   USERS: [
     {
       // 想要发送的人的名字
-      name: '老婆',
+      name: '我自己',
       // 使用微信测试号：扫码关注你的微信测试号后生成的一段字符串，在测试号后台能看到
       id: 'osC4I6opo03RdPf9my9ugx9y93bM',
       // 使用微信测试号：你想对他发送的模板消息的模板ID
-      useTemplateId: 'cjTKBlmjFDPt8GDwE8KB2Gt_r5OjB8Kx34pBJzmgtQM',
+      useTemplateId: 'aFw7FkDH2QBo_6eNZ5vCbMDWlC0Z2Rf7K8Uo2ealI_k',
+      // 新历生日, 仅用作获取星座运势, 格式必须为MM-DD
+      horoscopeDate: '08-06',
+      festivals: [
+        // 注意：此条配置日期为阴历日期，因为`type`中 “生日” 之前有 * 符号
+        {
+          type: '*生日', name: '老婆', year: '1992', date: '07-08',
+        },
+        // 注意：此条配置日期为阳历日期，因为`type`中 “生日” 之前没有 * 符号
+        {
+          type: '生日', name: '老婆', year: '1992', date: '08-06',
+        },
+        {
+          type: '节日', name: '相识纪念日', year: '2021', date: '05-29',
+        },
+      ],
+      // 我们在一起已经有xxxx天了的配置
+      customizedDateList: [
+        // 在一起的日子
+        { keyword: 'love_day', date: '2021-05-29' },
+        // 结婚纪念日
+        { keyword: 'marry_day', date: '2022-04-16' },
+      ],
+    },
+    {
+      // 想要发送的人的名字
+      name: '老婆',
+      // 使用微信测试号：扫码关注你的微信测试号后生成的一段字符串，在测试号后台能看到
+      id: 'osC4I6iqsydJcBi-JCL0Czd8VRGM',
+      // 使用微信测试号：你想对他发送的模板消息的模板ID
+      useTemplateId: 'aFw7FkDH2QBo_6eNZ5vCbMDWlC0Z2Rf7K8Uo2ealI_k',
       // 新历生日, 仅用作获取星座运势, 格式必须为MM-DD
       horoscopeDate: '08-06',
       festivals: [
